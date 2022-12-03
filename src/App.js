442adb186fc67ff1bc22737aec920066b729395d
Robel Fekadu <robelfekadu@gmail.com>
2022-12-03 20:57:00 +0300

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import './app.css';
 
 function App() {
-	return <h1>Hello World! This a react app without create-react-app</h1>;
+	return (
+		<h1 className="text-primary items-center font-bold text-4xl">
+			Hello World! This a react app without create-react-app
+		</h1>
+	);
 }
 export default App;
